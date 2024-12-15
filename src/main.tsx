84d798ad281c3622d25c02f7464f35e9a5b15488
Yaswanth Varada <yaswanthvarada20@gmail.com>
2024-12-15 17:34:49 +0530

    production issues fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import './index.css'
 import App from "./App.tsx";
@@ -8,7 +8,7 @@ import "primeicons/primeicons.css";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  // <StrictMode>
+  <App />
+  // </StrictMode>
 );
